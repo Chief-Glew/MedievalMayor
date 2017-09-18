8423ed2f86de4560b753eb17390e9e6f9ce13e36
joseph.brighton <joseph.brighton@fdmgroup.com>
2017-09-18 16:00:03 +0100

    TEST

--- src/test/java/com/fdmgroup/medievalmayor/resourceproducers/FarmTest.java
+++ src/test/java/com/fdmgroup/medievalmayor/resourceproducers/FarmTest.java
@@ -1,46 +1,29 @@
 package com.fdmgroup.medievalmayor.resourceproducers;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import com.fdmgroup.medievalmayor.game.city.City;
-import com.fdmgroup.medievalmayor.game.city.CityFactory;
 import com.fdmgroup.medievalmayor.game.exceptions.AssignedNegativeNumberException;
 import com.fdmgroup.medievalmayor.game.exceptions.InsufficentPopulationException;
 import com.fdmgroup.medievalmayor.game.resourceproducers.Farm;
-import com.fdmgroup.medievalmayor.game.resourceproducers.ResourceProducerService;
 
 public class FarmTest {
 	private Farm farm;
-	private Mine mine;
-	private ResourceProducerService buildingManager;
-	private City city;
-	private CityService cityService;
-	private CityFactory cityFactory;
-
+	
 	
 	@Before
 	public void init(){
-		cityFactory = new CityFactory();
-		cityService = CityService.getInstance();
-		city = cityFactory.getNewCity();
-		buildingManager = ResourceProducerService.getInstance();
+	
 	}
 	
 	@Test
 	public void testThatWhenOnePersonIsAssignedToAFarmThreeFoodIsProduced() throws InsufficentPopulationException, AssignedNegativeNumberException{
-		cityService.assignPeopleToFarm(city, 1);
-		cityService.updateTurn(city);
-		assertEquals(3,farm.produceResource());
+	
 	}
 	
 	@Test
 	public void testThatWhenTwoPeopleIsAssignedToAFarmSixFoodIsProduced() throws InsufficentPopulationException, AssignedNegativeNumberException{
-		cityService.assignPeopleToFarm(city, 2);
-		cityService.updateTurn(city);
-		assertEquals(6,farm.produceResource());
+	
 	}
 	
 	
