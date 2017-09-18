798c1ef4a4b3eb1598649f9ee941835912dfe591
liam.fleming <liam.fleming@fdmgroup.com>
2017-09-18 17:00:26 +0100

    AdminCommandTest

--- src/test/java/com/fdmgroup/medievalmayor/game/command/AdminCommandTest.java
+++ src/test/java/com/fdmgroup/medievalmayor/game/command/AdminCommandTest.java
@@ -1,5 +1,52 @@
 package com.fdmgroup.medievalmayor.game.command;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.fdmgroup.medievalmayor.config.AppConfig;
+import com.fdmgroup.medievalmayor.game.city.City;
+import com.fdmgroup.medievalmayor.game.city.CityFactory;
+import com.fdmgroup.medievalmayor.game.resourceproducers.Farm;
+import com.fdmgroup.medievalmayor.game.resourceproducers.Forest;
+import com.fdmgroup.medievalmayor.game.resourceproducers.LumberMill;
+import com.fdmgroup.medievalmayor.game.resourceproducers.ResourceProducer;
+
 public class AdminCommandTest {
 
+	private AdminCommand adminCommand;
+	private ResourceProducer resourceProducer;
+	private CityFactory cityFactory;
+	private City city;
+	
+	@Before
+	public void init(){
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		adminCommand = new AdminCommand(); 
+		resourceProducer = new Forest(5);
+		cityFactory = applicationContext.getBean(CityFactory.class);
+		city = cityFactory.getNewCity();
+
+	}
+	
+	@Test
+	public void testThatSetBaseResourceProductionForResourceForestCanBeSetToTen(){
+		adminCommand.setBaseResourceProductionForResourceProducer(resourceProducer, 10);
+		assertEquals(10, resourceProducer.getBaseResourceProduction());
+	}
+	
+	@Test
+	public void testThatTheAmountOfLumberPerWoodCanBeChangedToOne(){
+		adminCommand.setAmmountOfLumberPerWoodForLumberMill((LumberMill) city.getResourceProducerOfType(LumberMill.class), 1);
+		assertEquals(((LumberMill) city.getResourceProducerOfType(LumberMill.class)).getAmountOfLumberPerWood(), 1);
+	} //TODO get rid of casting!!!
+
+	@Test
+	public void testThatUpgradeMultiplierChangesTheMultiplierOfAFarmToFive(){
+		adminCommand.setUpgradeMultiplierForResourceProducer(city.getResourceProducerOfType(Farm.class), 5);
+		assertEquals(city.getResourceProducerOfType(Farm.class).getUpgradeMultiplier(), 5);
+	}
 }
